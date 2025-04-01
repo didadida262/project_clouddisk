4d07ab66fc3d1bc69730c7477856af7076307e39
miles <719713496@qq.com>
2025-04-01 14:03:13 +0800

    add: 路径选择

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,6 +5,9 @@ interface IProps {
 export default function MainPage(props: IProps) {
     const {currentpath} = props
     return (
+        <div className="w-full h-full">
           <span className='text-[white]'>当前路径{currentpath }</span>
+
+        </div>
     )
 }

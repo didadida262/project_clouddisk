74096f79f1b2fc0ea313b0fe45cd269b55332058
miles <719713496@qq.com>
2025-04-02 11:45:28 +0800

    add:video_operation

--- src/components/VideoContainer.tsx
+++ src/components/VideoContainer.tsx
@@ -1,23 +1,37 @@
 import { useEffect } from 'react'
 import { useResources } from '../provider/resource-context'
+import { Button } from 'antd'
 
 export default function VideoContainer() {
   const { currentfileurl } = useResources()
+
+  const handleNext = () => {}
+  const handlePlayMode = () => {}
 
   useEffect(() => {
     console.log('selectedFile>>>>change', currentfileurl)
   }, currentfileurl)
 
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <video
-        muted
-        className="w-full h-full object-fit"
-        autoPlay
-        loop
-        controls
-        src={currentfileurl}
-      />
+    <div className="w-full h-full flex justify-between items-center flex-col">
+      <div className="video w-full h-[calc(100%_-_55px)] selectedG flex justify-center items-center">
+        <video
+          muted
+          className="w-full h-full object-fit"
+          autoPlay
+          loop
+          controls
+          src={currentfileurl}
+        />
+      </div>
+      <div className="operation w-full h-[50px] flex justify-start items-center gap-x-[20px]">
+        <Button type="primary" onClick={handlePlayMode}>
+          顺序播放
+        </Button>
+        <Button type="primary" onClick={handleNext}>
+          下一首
+        </Button>
+      </div>
     </div>
   )
 }
